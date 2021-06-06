50a7bfb0dd11c5c23584d4241d2ff21219016add
Rajesh Kanna <75661804+rajeshias@users.noreply.github.com>
2021-06-06 23:53:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,13 @@ output_path = ''                                # leave blank for using the same
 options = webdriver.ChromeOptions()
 # options.add_argument('--headless')
 options.add_argument("--disable-gpu")
+options.add_argument("start-maximized")
+options.add_argument("enable-automation")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-infobars")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-browser-side-navigation")
+
 # options.add_argument("--start-maximized")
 driver = webdriver.Chrome(chrome_driver_location, options=options)
 
